c6dc4ec7e9ea50f207e6d09f9924b62a85c5ab07
Yongyuan Yan <yongyuay@usc.edu>
2021-02-02 16:00:06 -0800

    added boundaries

--- Farm Simulation/Assets/Scripts/PlayerMovement.cs
+++ Farm Simulation/Assets/Scripts/PlayerMovement.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -9,6 +10,14 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody2D rb2d;        //Store a reference to the Rigidbody2D component required to use 2D Physics.
 
     public Joystick joystick;
+
+    private float minX = -7.9f;
+    private float minY = -4f;
+
+    private float maxX = 7.9f;
+    private float maxY = 4f;
+
+    private float joystickDeadZone = 0.2f;
     
     // Use this for initialization
     void Start()
@@ -23,17 +32,34 @@ public class PlayerMovement : MonoBehaviour
         //Store the current horizontal input in the float moveHorizontal.
         // float moveHorizontal = Input.GetAxis ("Horizontal");
         float moveHorizontal = 0f;
-        if(joystick.Horizontal >= 0.2f || joystick.Horizontal <= -0.2f){
-        	moveHorizontal = joystick.Horizontal;
+        // if the joystick falls under a joystickdeadzone, there are no reactions
+        if(Math.Abs(joystick.Horizontal) >= joystickDeadZone){
+            // if positive horizontal movements are within scope, allow the movements
+            if(rb2d.position.x < maxX && joystick.Horizontal > 0){
+                moveHorizontal = joystick.Horizontal;
+                // if negative horizontal movements are within scope, allow the movements
+            }else if(rb2d.position.x > minX && joystick.Horizontal < 0){
+                moveHorizontal = joystick.Horizontal;
+            }
         }
+        
+        
         
 
         //Store the current vertical input in the float moveVertical.
         // float moveVertical = Input.GetAxis ("Vertical");
         float moveVertical = 0f;
-        if(joystick.Vertical >= 0.2f || joystick.Vertical <= -0.2f){
-        	moveVertical = joystick.Vertical;
+        // if the joystick falls under a joystickdeadzone, there are no reactions
+        if(Math.Abs(joystick.Vertical) >= joystickDeadZone){
+            // if positive vertical movements are within scope, allow the movements
+            if(rb2d.position.y < maxY && joystick.Vertical > 0){
+                moveVertical = joystick.Vertical;
+                // if negative vertical movements are within scope, allow the movements
+            }else if(rb2d.position.y > minY && joystick.Vertical < 0){
+                moveVertical = joystick.Vertical;
+            }
         }
+        
         
         //Use the two store floats to create a new Vector2 variable movement.
         Vector2 movement = new Vector2 (moveHorizontal, moveVertical);
@@ -41,4 +67,5 @@ public class PlayerMovement : MonoBehaviour
         //Call the AddForce function of our Rigidbody2D rb2d supplying movement multiplied by speed to move our player.
         rb2d.velocity =  (movement * speed);
     }
+
 }
