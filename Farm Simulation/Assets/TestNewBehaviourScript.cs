e59f62b8b39d83a51d900ecd621e4d1c933e020f
Yongyuan Yan <yongyuay@usc.edu>
2021-02-08 17:36:30 -0800

    Before I change scale

---
+++ Farm Simulation/Assets/TestNewBehaviourScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class TestNewBehaviourScript : MonoBehaviour
+{
+
+	public Transform target;
+	private NavMeshAgent agent;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    	target = GameObject.FindGameObjectWithTag("TestTarget").transform;
+    	agent = GetComponent<NavMeshAgent>();
+    	agent.updateRotation = false;
+    	agent.updateUpAxis = false;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    	// agent.Warp(agent.transform);
+    	agent.SetDestination(target.position);
+        
+    }
+}
