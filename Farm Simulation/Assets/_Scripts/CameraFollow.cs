b2e5688fe639dc01e8ab9aea1663ca776a8ff882
josh <kim777@usc.edu>
2021-02-16 19:43:11 -0800

    Fungus and cam bound

--- Farm Simulation/Assets/_Scripts/CameraFollow.cs
+++ Farm Simulation/Assets/_Scripts/CameraFollow.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
@@ -7,41 +9,62 @@ public class CameraFollow : MonoBehaviour
 
     public float smoothFactor;
     public Vector3 offset;
-    public Vector2 minPosition;
-    public Vector2 maxPosition;
+
+    [SerializeField]
+    float leftLimit;
+    [SerializeField]
+    float rightLimit;
+    [SerializeField]
+    float bottomLimit;
+    [SerializeField]
+    float topLimit;
+
     Vector3 desiredPosition;
 
     // Use this for initialization
     void Start()
     {
-        desiredPosition = target.position;
+    //    desiredPosition = target.position;
     }
 
     // Update is called once per frame
 
     void Update()
     {
+        Vector3 targetPosition = transform.position;
+        Vector3 desiredPosition = target.transform.position + offset;
+        Vector3 smoothedPosition = Vector3.Lerp(targetPosition, desiredPosition, smoothFactor * Time.deltaTime);
+        transform.position = smoothedPosition;
 
+        transform.position = new Vector3
+            (
+                Mathf.Clamp(transform.position.x, leftLimit, rightLimit),
+                Mathf.Clamp(transform.position.y, bottomLimit, topLimit),
+                transform.position.z
+
+            );
     }
 
     // called every fixed frame rate
     void FixedUpdate()
     {
-        Vector3 desiredPosition = target.position + offset;
-        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothFactor * Time.deltaTime);
-        transform.position = smoothedPosition;
-
+        
         //   transform.LookAt(target);
     }
     // called after update and fixed update
     void LateUpdate()
     {
-        if (transform.position != target.position)
-        {
-            Vector3 desiredPosition = new Vector3(target.position.x, target.position.y, transform.position.z);
-            desiredPosition.x = Mathf.Clamp(desiredPosition.x, minPosition.x, maxPosition.x);
-            desiredPosition.y = Mathf.Clamp(desiredPosition.y, minPosition.y, maxPosition.y);
-        }
+   
+    }
+
+    void onDrawGizmos()
+    {
+        // draw around the camera boundary
+        Gizmos.color = Color.red;
+
+        Gizmos.DrawLine(new Vector2(leftLimit, topLimit), new Vector2(rightLimit, topLimit));
+        Gizmos.DrawLine(new Vector2(rightLimit, topLimit), new Vector2(rightLimit, bottomLimit));
+        Gizmos.DrawLine(new Vector2(rightLimit, bottomLimit), new Vector2(leftLimit, bottomLimit));
+        Gizmos.DrawLine(new Vector2(leftLimit, bottomLimit), new Vector2(leftLimit, topLimit));
     }
 }
-
