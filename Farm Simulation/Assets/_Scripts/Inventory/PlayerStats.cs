32dd0a3c944512695990358a47b156f4771ea86f
josh <kim777@usc.edu>
2021-04-14 12:27:39 -0700

    additional detail

--- Farm Simulation/Assets/_Scripts/Inventory/PlayerStats.cs
+++ Farm Simulation/Assets/_Scripts/Inventory/PlayerStats.cs
@@ -16,7 +16,7 @@ public static class PlayerStats
         set { harvest = value;  }
     }
 
-    private static bool toSave = false;
+    private static bool toSave = true;
 
     public static bool Save
     {
