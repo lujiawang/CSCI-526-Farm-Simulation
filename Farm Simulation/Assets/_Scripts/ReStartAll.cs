a4b7ec109174db2e648ce4f112d2ddfd73a526ca
Yongyuan Yan <yongyuay@usc.edu>
2021-04-17 07:30:31 -0700

    Debug: Now selecting new game from mainmenu scene won't reset volumes

--- Farm Simulation/Assets/_Scripts/ReStartAll.cs
+++ Farm Simulation/Assets/_Scripts/ReStartAll.cs
@@ -6,7 +6,18 @@ public class ReStartAll : MonoBehaviour
 {
     public void ReStart()
     {
+    	float musciVol = 1f;
+    	float SFXVol = 1f;
+    	if(PlayerPrefs.HasKey("MusicVolume"))
+    		musciVol = PlayerPrefs.GetFloat("MusicVolume");
+    	if(PlayerPrefs.HasKey("SFXVolume"))
+    		SFXVol = PlayerPrefs.GetFloat("SFXVolume");
+
     	PlayerPrefs.DeleteAll();
+
+    	PlayerPrefs.SetFloat("MusicVolume", musciVol);
+    	PlayerPrefs.SetFloat("SFXVolume", SFXVol);
+
     	Inventory inventory = Inventory.instance;
     	inventory.ReStart();
     	StoreInventory storeInventory = StoreInventory.instance;
