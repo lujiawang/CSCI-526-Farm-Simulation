b11b81e77ffee8c856143f8dc99dc8211d08cc2e
Yongyuan Yan <yongyuay@usc.edu>
2021-04-19 06:12:19 -0700

    Added Credits

---
+++ Farm Simulation/Assets/_Scripts/Credits.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Credits : MonoBehaviour
+{
+    public void ShowCredits()
+    {
+    	FadeObj fadeScript = GameObject.Find("SceneCanvas").GetComponent<FadeObj>();
+    	Transform panel = this.transform;
+    	StartCoroutine(fadeScript.Fade(panel, true, 0.4f));
+
+    	// ZoomObj zoomScript = GameObject.Find("SceneCanvas").GetComponent<ZoomObj>();
+    	// StartCoroutine(zoomScript.Zoom(panel, true, 0.4f));
+    	// this.GetComponent<CanvasGroup>().alpha = 1f;
+    	this.GetComponent<CanvasGroup>().blocksRaycasts = true;
+    }
+    public void HideCredits()
+    {
+    	FadeObj fadeScript = GameObject.Find("SceneCanvas").GetComponent<FadeObj>();
+    	Transform panel = this.transform;
+    	StartCoroutine(fadeScript.Fade(panel, false, 0.4f));
+    	// this.GetComponent<CanvasGroup>().alpha = 0f;
+    	this.GetComponent<CanvasGroup>().blocksRaycasts = false;
+    }
+}
